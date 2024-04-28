bfc22d521b64c811c531ca2d66991e89a22bb307
Praneesh <parshipraneesh8@gmail.com>
2024-04-28 09:17:28 +0530

    vercel

--- client/src/urlContext.js
+++ client/src/urlContext.js
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export const urlContext = createContext("https://octopus-app-ocqjn.ondigitalocean.app/");
+export const urlContext = createContext("https://the-coder-companion-server.vercel.app/");
